b8a70a1c14311b811f35d6bfb16a74c42000f405
Muhammed Irshad VP <118966759+irshad800@users.noreply.github.com>
2025-06-28 15:13:07 +0400

    updated email utils file

--- utils/cryptoCCAvenue.js
+++ utils/cryptoCCAvenue.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const crypto = require('crypto');
 const iv = '0123456789abcdef';
 
@@ -15,21 +14,3 @@ exports.decrypt = (encText, workingKey) => {
   decrypted += decipher.final('utf8');
   return decrypted;
 };
-=======
-const crypto = require('crypto');
-const iv = '0123456789abcdef';
-
-exports.encrypt = (plainText, workingKey) => {
-  const cipher = crypto.createCipheriv('aes-128-cbc', workingKey, iv);
-  let encrypted = cipher.update(plainText, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted;
-};
-
-exports.decrypt = (encText, workingKey) => {
-  const decipher = crypto.createDecipheriv('aes-128-cbc', workingKey, iv);
-  let decrypted = decipher.update(encText, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
-};
->>>>>>> 45d1eeeec13fa94e11488e703d8349afb96a9770
